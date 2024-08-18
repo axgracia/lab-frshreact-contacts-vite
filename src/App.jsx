aea3d269e276492eca4a6cdab3d437b830843ac0
axartmiami@gmail.com <axartmiami@gmail.com>
2024-08-18 16:27:58 -0400

    added contact list in table

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,43 @@
 import "./App.css";
+import { useState } from "react";
+import contacts from "./contacts.json"; 
+
 
 function App() {
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
+      <ContactList />
     </div>
   );
 }
+
+const ContactList = () => {
+  // Step 1: Initialize state with an array of 5 contacts
+  const [contactList] = useState(contacts.slice(0, 5));
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+        </tr>
+      </thead>
+      <tbody>
+        {contactList.map((contact, index) => (
+          <tr key={index}>
+            <td>
+              <img src={contact.pictureUrl} alt={contact.name} width="50" />
+            </td>
+            <td>{contact.name}</td>
+            <td>{contact.popularity}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
 
 export default App;
